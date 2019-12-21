c709dcac7bf1e502cda1905b10da4575804ebfb4
Chris Sciora <zdigriz@gomobileiq.com>
2019-12-20 20:18:22 -0700

    Add /phpinfo to repo

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         array('description' => 'bar', 'url' => './bar/'),
         array('description' => 'fleetmanager', 'url' => './fleetmanager/dist/'),
         array('description' => 'laravel', 'url' => './laravel/public/'),
+        array('description' => 'phpinfo', 'url' => './phpinfo/'),
         array('description' => 'todolist', 'url' => './todolist/'),
 
     );
