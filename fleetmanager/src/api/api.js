12e4c08069289aae77a2dd62a79cf05b7dd6fbc2
Chris Sciora <zdigriz@gomobileiq.com>
2020-01-19 22:40:08 -0700

    Add /public folder to project

--- fleetmanager/src/api/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const baseURL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8080/';
-export default axios.create({
-  baseURL,
-});
--- fleetmanager/src/api/index.js
+++
@@ -1,8 +0,0 @@
-import api from './api';
-import tableData from './table-data';
-
-export const getData = () => api.get('api').then((response) => console.log(response));
-
-export const getMapData = async () => tableData;
-
-export const getOrders = async () => tableData;
